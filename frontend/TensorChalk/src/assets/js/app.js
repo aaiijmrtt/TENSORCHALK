b15f21b0337725d0ddc399010f3531f8a1f175b9
Vivek Roy <vivekroyandroid@gmail.com>
2017-06-30 16:49:07 +0530

    Make sidebar scrollable

--- frontend/TensorChalk/src/assets/js/app.js
+++ frontend/TensorChalk/src/assets/js/app.js
@@ -1,1 +1,8 @@
+$(document).ready(function(){
+    var dH = $(window).height() - $("#navbar").outerHeight();
+    $("#index-body-div, #api-elems").css({
+        height: dH
+    });
+    console.log(dH);
+});
 $(document).foundation();
